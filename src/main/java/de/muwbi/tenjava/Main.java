46ff1aceeb7889de72a1d1b9b81d3dc56217e901
Muwbi <me@muwbi.de>
2013-12-06 15:21:06 +0100

    Skeleton

---
+++ src/main/java/de/muwbi/tenjava/Main.java
@@ -0,0 +1,13 @@
+package de.muwbi.tenjava;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+// This will be renamed later ;)
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
